819f76cb5da16db288c330299162bfb3b697e8f6
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-06-15 17:37:27 -0700

    project complete

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,32 @@
+const Shippers = require('./shippers-model')
+
 module.exports = {
   checkId,
   checkPayload,
 }
 
 function checkId(req, res, next) {
+  const { id } = req.params
+
+  Shippers.getById(id)
+    .then(id => {
+      if (!id) {
+        next({status: 404, message: "that id does not exist"})
+      }
+    })
   next()
 }
 
 function checkPayload(req, res, next) {
+  const { phone, shippername } = req.body;
+
+  if (!phone || 
+    phone === undefined || 
+    !shippername || 
+    shippername === undefined) 
+    {
+      next({status: 404, message: "All fields required"})
+  }
+
   next()
 }
